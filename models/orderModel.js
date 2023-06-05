1e13c82883723a545a63b25827c63981bc53b2b3
marcogiu <48352400+marcogiu@users.noreply.github.com>
2023-06-05 13:15:30 +0200

    update

--- models/orderModel.js
+++ models/orderModel.js
@@ -4,6 +4,12 @@ const orderSchema = mongoose.Schema({
   products: {
     type: [Schema.Types.ObjectId],
   },
+  orders: {
+    type: [Schema.types.ObjectId],
+  },
+  date: {
+    type: Date,
+  },
 });
 
 module.exports = mongoose.model("Order", orderSchema);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+connectDb();
 const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
