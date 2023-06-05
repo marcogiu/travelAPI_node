c995cf516924779f2d6ff058be6b99ebc04ff7b3
marcogiu <48352400+marcogiu@users.noreply.github.com>
2023-06-05 18:56:12 +0200

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,45 +1,70 @@
 const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
 
 // @desc Get all products
 // @route GET api/products
 // access public
-const getProducts = async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find();
   res.status(200).json(products);
-};
+});
 
 // @desc Create new product
 // @route POST api/products
 // access public
-const createProduct = async (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
   const { name } = req.body;
-  if (!name) throw "Invalid";
+  if (!name) {
+    res.status(400);
+    throw new Error("Invalid");
+  }
   const product = await Product.create({
     name,
   });
   res.status(201).json(product);
-};
+});
 
 // @desc Get a product
 // @route GET api/products/:id
 // access public
-const getProduct = async (req, res) => {
-  res.status(200).json({ message: "visualizza un prodotto" });
-};
+const getProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  res.status(200).json(product);
+});
 
 // @desc Get all products
 // @route GET api/products
 // access public
-const updateProduct = async (req, res) => {
-  res.status(200).json({ message: "update prodotto" });
-};
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedProduct);
+});
 
 // @desc Get all products
 // @route GET api/products
 // access public
-const deleteProduct = async (req, res) => {
-  res.status(200).json({ message: "delete prodotto" });
-};
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  await product.deleteOne();
+  res.status(200).json(product);
+});
 
 module.exports = {
   getProducts,
